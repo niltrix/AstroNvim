30609d995fed1eefe387ee24c6e12543d768bf73
niltrix <axlgilby@gmail.com>
2022-07-24 23:49:33 +0900

    add catppuccin theme..

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -164,6 +164,102 @@ local config = {
       --     }
       --   end,
       -- },
+      {
+        "catppuccin/nvim",
+        config = function()
+          require("catppuccin").setup {
+            dim_inactive = {
+		          enabled = false,
+		          shade = "dark",
+		          percentage = 0.15,
+	          },
+	          transparent_background = false,
+	          term_colors = false,
+	          compile = {
+		          enabled = false,
+		          path = vim.fn.stdpath "cache" .. "/catppuccin",
+	          },
+	          styles = {
+		          comments = { "italic" },
+		          conditionals = { "italic" },
+		          loops = {},
+		          functions = {},
+		          keywords = {},
+		          strings = {},
+		          variables = {},
+		          numbers = {},
+		          booleans = {},
+		          properties = {},
+		          types = {},
+		          operators = {},
+	          },
+	          integrations = {
+		          treesitter = true,
+		          native_lsp = {
+			          enabled = true,
+			          virtual_text = {
+				          errors = { "italic" },
+				          hints = { "italic" },
+				          warnings = { "italic" },
+				          information = { "italic" },
+			          },
+			          underlines = {
+				          errors = { "underline" },
+				          hints = { "underline" },
+				          warnings = { "underline" },
+				          information = { "underline" },
+			          },
+		          },
+		          coc_nvim = false,
+		          lsp_trouble = false,
+		          cmp = true,
+		          lsp_saga = false,
+		          gitgutter = false,
+		          gitsigns = true,
+		          leap = false,
+		          telescope = true,
+		          nvimtree = {
+			          enabled = true,
+			          show_root = true,
+			          transparent_panel = false,
+		          },
+		          neotree = {
+			          enabled = false,
+			          show_root = true,
+			          transparent_panel = false,
+		          },
+		          dap = {
+			          enabled = false,
+			          enable_ui = false,
+		          },
+		          which_key = false,
+		          indent_blankline = {
+			          enabled = true,
+			          colored_indent_levels = false,
+		          },
+		          dashboard = true,
+		          neogit = false,
+		          vim_sneak = false,
+		          fern = false,
+		          barbar = false,
+		          bufferline = true,
+		          markdown = true,
+		          lightspeed = false,
+		          ts_rainbow = false,
+		          hop = false,
+		          notify = true,
+		          telekasten = true,
+		          symbols_outline = true,
+		          mini = false,
+		          aerial = false,
+		          vimwiki = true,
+		          beacon = true,
+	          },
+	          color_overrides = {},
+	          custom_highlights = {},
+          }
+        end,
+      },
       -- disable plugins:
       ["declancm/cinnamon.nvim"] = { disable = true }, -- Slow scrolling
     },
@@ -295,6 +391,9 @@ local config = {
   -- This function is run last
   -- good place to configure mappings and vim options
   polish = function()
+    -- color scheme
+    vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
+    vim.cmd [[colorscheme catppuccin]]
     -- Set key bindings
     vim.keymap.set("n", "<C-s>", ":w!<CR>")
     vim.api.nvim_set_var("python3_host_prog", "/Users/ibyeongjin/.pyenv/versions/pynvim/bin/python3")
